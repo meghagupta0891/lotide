d88edea0c2827d895569a566e92a29b049cda8ee
Megha <meghagupta0891@gmail.com>
2021-01-08 08:37:31 +0000

    Added asserEqual method

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+   if(actual === expected) {
+       console.log("Assertion passed : " + actual + " = " + expected);
+   }
+   else {
+    console.log("Assertion failed : " + actual + " !== " + expected)
+   }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1,"1");
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
